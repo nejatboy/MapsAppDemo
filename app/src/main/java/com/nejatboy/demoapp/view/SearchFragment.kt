fdc3b7ef15217142e3a437071c97d1a30a1c7b49
Nejat Boy <nejat219@hotmail.com>
2020-06-13 19:34:39 +0300

    Konum alındı

--- app/src/main/java/com/nejatboy/demoapp/view/SearchFragment.kt
+++ app/src/main/java/com/nejatboy/demoapp/view/SearchFragment.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -19,13 +20,16 @@ import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.nejatboy.demoapp.R
+import com.nejatboy.demoapp.model.Location
 import com.nejatboy.demoapp.viewmodel.SearchViewModel
+import kotlinx.android.synthetic.main.fragment_search.view.*
 import kotlin.system.exitProcess
 
 
 class SearchFragment : Fragment() {
 
     private lateinit var viewModel: SearchViewModel
+    private var currentLocation: Location? = null
 
 
     override fun onCreateView(
@@ -44,7 +48,15 @@ class SearchFragment : Fragment() {
 
         observeLiveData()
 
-        viewModel.runData()
+        //viewModel.runData()
+
+        view.buttonTestSearch.setOnClickListener {
+            currentLocation?.let {
+                println("Konum var api isteği yapılır")
+            } ?: run {
+                println("Konum alınmamış sıkıntı var")
+            }
+        }
     }
 
 
@@ -75,12 +87,15 @@ class SearchFragment : Fragment() {
             }
         })
 
-
         viewModel.places.observe(viewLifecycleOwner, Observer {
             for (place in it) {
                 //println(place.name)
                 //println(place.scope)
             }
+        })
+
+        viewModel.currentLocation.observe(viewLifecycleOwner, Observer {
+            currentLocation = it
         })
     }
 
@@ -90,9 +105,9 @@ class SearchFragment : Fragment() {
             context?.let {context ->
                 if (checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                     ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
-                    println("İLK KEZ SORARIM")
-                } else {
-                    println("İZİNİM ZATEN VAR ÇAIŞIRIM ")
+
+                } else {        //İzin var
+                    viewModel.getCurrentLocation()
                 }
             }
         }
@@ -105,7 +120,7 @@ class SearchFragment : Fragment() {
             if (grantResults.isNotEmpty()) {
                 context?.let {context ->
                     if (checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {     //İlk izin verilmesi durumu
-                        println("İLK İZİN VERİLDİĞİNDE ÇALIŞIRIM")
+                        viewModel.getCurrentLocation()
                     } else {
                         exitProcess(0)
                     }
--- app/src/main/java/com/nejatboy/demoapp/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/nejatboy/demoapp/viewmodel/SearchViewModel.kt
@@ -1,23 +1,17 @@
 package com.nejatboy.demoapp.viewmodel
 
-import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
-import android.content.DialogInterface
-import android.content.Intent
+import android.location.LocationListener
 import android.location.LocationManager
-import android.provider.Settings
-import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.getSystemService
+import android.os.Bundle
 import androidx.lifecycle.MutableLiveData
-import com.nejatboy.demoapp.model.Comment
 import com.nejatboy.demoapp.model.Location
 import com.nejatboy.demoapp.model.Place
 import com.nejatboy.demoapp.service.MyAPIService
 import kotlinx.coroutines.launch
 import java.lang.Exception
-import kotlin.system.exitProcess
 
 class SearchViewModel(application: Application) : BaseViewModel(application) {
 
@@ -29,7 +23,7 @@ class SearchViewModel(application: Application) : BaseViewModel(application) {
     val currentLocation = MutableLiveData<Location>()
 
 
-    fun runData() {
+    fun getData() {
         getDataFromAPI()
     }
 
@@ -58,6 +52,12 @@ class SearchViewModel(application: Application) : BaseViewModel(application) {
     }
 
 
+    @SuppressLint("MissingPermission")
+    fun getCurrentLocation() {
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1, 1f, locationListener)   //Konum al
+    }
+
+
     private fun getDataFromAPI () {
         launch {
             val response = MyAPIService().getDataFromAPI()
@@ -70,5 +70,23 @@ class SearchViewModel(application: Application) : BaseViewModel(application) {
     }
 
 
-    
+    private val locationListener = object : LocationListener {
+        override fun onLocationChanged(location: android.location.Location?) {
+            location?.let {
+                currentLocation.value = Location(it.latitude, it.longitude)
+                println(it.latitude)
+                println(it.longitude)
+            }
+        }
+
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+        }
+
+        override fun onProviderEnabled(provider: String?) {
+        }
+
+        override fun onProviderDisabled(provider: String?) {
+        }
+
+    }
 }
